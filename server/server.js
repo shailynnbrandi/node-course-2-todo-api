e80c1da4d3623e2b50d9524bb2232f58a9f21112
Shailynn Race <shailynnbrandi@ygmail.com>
2018-10-12 09:44:41 -0400

    resubmitting files for updates issue

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require ('./models/todo');
 var {User} = require('./models/user')
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -54,8 +55,20 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () =>{
-  console.log('Started on port 3000')
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user)
+  }).catch((e) => {
+    res.status(400).send(e);
+
+  })
+}); 
+
+app.listen(port, () =>{
+  console.log(`Started on port ${port}`)
 })
 
 module.exports = {app};
