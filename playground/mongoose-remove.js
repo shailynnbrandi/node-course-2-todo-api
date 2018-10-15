6a7774545adb580e39532dd8d75dc26aea702e9f
Shailynn Race <shailynnbrandi@ygmail.com>
2018-10-15 13:32:28 -0400

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,17 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require ('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) =>{
+//   console.log(result);
+// });
+
+Todo.findOneAndRemove({_id:'5bc4c8e8dd1a0d4319f3b5dd'}).then((todo) => {
+
+});
+
+// Todo.findByIdAndRemove('5bc4c8e8dd1a0d4319f3b5dd').then((todo)=>{
+//   console.log(todo);
+// })
