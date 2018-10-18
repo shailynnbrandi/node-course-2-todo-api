f5c8f5bce973802dd464fd655a916732983df728
Shailynn Race <shailynnbrandi@ygmail.com>
2018-10-18 14:17:14 -0400

    Uploading for debugging

--- server/server.js
+++ server/server.js
@@ -116,6 +116,26 @@ user.save().then(() => {
   });
 });
 
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  }).catch((e) => {
+    res.status(401).send();
+  });
+};
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
 app.listen(port, () =>{
   console.log(`Started on port ${port}`)
 })
