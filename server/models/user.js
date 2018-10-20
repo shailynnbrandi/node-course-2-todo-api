1b6b37f159191ad029a3fd7f8e05942a24ad8538
Shailynn Race <shailynnbrandi@ygmail.com>
2018-10-20 11:41:38 -0400

    HASH user poassword before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require ('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require ('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   
@@ -59,6 +60,21 @@ var UserSchema = new mongoose.Schema({
       'tokens.access': 'auth'
     });
   };
+
+  UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          user.password = hash;
+          next();
+  });
+});  
+    }else {
+      next();
+    }
+  });
 
 var User = mongoose.model('User', UserSchema);
 
